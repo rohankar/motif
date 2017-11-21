9e7b8ad1e1e7fafa0ed815d1c0053c5cc76fa9c4
DimitriosHytiroglou <DimitriosHytiroglou@users.noreply.github.com>
2017-11-20 22:10:41 -0800

    Updated to add all Entities and Sentiment to CSV

--- JSON_to_Dict_Sentiment.py
+++ JSON_to_Dict_Sentiment.py
@@ -1,27 +1,42 @@
 import json
 from pprint import pprint
 from operator import itemgetter
+import csv
 
-data = json.load(open('Sentiment\\1.json'))
+writer = open('Sentiment\\All_Sentiments.txt','w') 
+	#with open('Sentiment\\All_Sentiments.csv','w') as SentimentFile:
+#writer = csv.writer(SentimentFile)
 
-#Entities = []
-
-#pprint(len(data["entities"]))
-#for i in range(len(data["entities"])):
-#	print(i)
-#	ent = data["entities"][i]["name"]
-#	sal = data["entities"][i]["salience"]
-#	Entities.append([ent,sal])
-
-print(data["documentSentiment"]["magnitude"])
-print(data["documentSentiment"]["score"])
-
-#Entities = sorted(Entities, key=itemgetter(1))
-
-#print(Entities)
-
-#EntLen = len(Entities)
-#EntitiesTop5 = [Entities[EntLen-1],Entities[EntLen-2],Entities[EntLen-3],Entities[EntLen-4],Entities[EntLen-5]]
-
-#print()
-#print(EntitiesTop5)
+for i in range(123):
+	print(i+1)
+	data = json.load(open('Sentiment\\'+str(i+1)+'.json'))
+	
+	Sentiment = []
+	SentimentOnly = []
+	
+	data = json.load(open('Sentiment\\'+str(i+1)+'.json'))
+	
+	#Entities = []
+	
+	#pprint(len(data["entities"]))
+	#for i in range(len(data["entities"])):
+	#	print(i)
+	#	ent = data["entities"][i]["name"]
+	#	sal = data["entities"][i]["salience"]
+	#	Entities.append([ent,sal])
+	
+	mag = data["documentSentiment"]["magnitude"]
+	score = data["documentSentiment"]["score"]
+	print(str(score))
+	
+	#Entities = sorted(Entities, key=itemgetter(1))
+	
+	#print(Entities)
+	
+	#EntLen = len(Entities)
+	#EntitiesTop5 = [Entities[EntLen-1],Entities[EntLen-2],Entities[EntLen-3],Entities[EntLen-4],Entities[EntLen-5]]
+	
+	#print()
+	#print(EntitiesTop5)
+	
+	writer.write(str(score)+'\n')
