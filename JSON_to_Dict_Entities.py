428a2ebd24676401195a9c0d686c545ae45fb1a5
DimitriosHytiroglou <DimitriosHytiroglou@users.noreply.github.com>
2017-11-20 22:45:53 -0800

    Added overall Sentiment from Google API and google entities, however they prove chaotic and we will need to see if we keep them or not

--- JSON_to_Dict_Entities.py
+++ JSON_to_Dict_Entities.py
@@ -3,33 +3,35 @@ from pprint import pprint
 from operator import itemgetter
 import csv
 
-with open('Entities\\All_Entities.csv','w') as EntityFile:
-	writer = csv.writer(EntityFile)
-	i=1
-#for i in range(123):
+writer = open('Entities\\All_Entities.txt','w')
+#writer = csv.writer(EntityFile)
+#i=1
+for i in range(123):
 	print(i+1)
 	data = json.load(open('Entities\\'+str(i+1)+'.json'))
 
 	Entities = []
-	EntitiesOnly = []
+	#EntitiesOnly = []
+	EntitiesOnly = ''
 
 	pprint(len(data["entities"]))
-	for i in range(len(data["entities"])):
-		print(i)
-		ent = data["entities"][i]["name"]
-		sal = data["entities"][i]["salience"]
+	for j in range(len(data["entities"])):
+		print(j)
+		ent = data["entities"][j]["name"]
+		sal = data["entities"][j]["salience"]
 		Entities.append([ent,sal])
-		EntitiesOnly.append(ent)
+		EntitiesOnly = EntitiesOnly+','+ent
+		#EntitiesOnly.append(ent)
 		
-	print(Entities)
+	#print(Entities)
 
-	Entities = sorted(Entities, key=itemgetter(1))
+	#Entities = sorted(Entities, key=itemgetter(1))
 
-	print(Entities)
+	#print(Entities)
 
-	EntLen = len(Entities)
-	EntitiesTop5 = [Entities[EntLen-1],Entities[EntLen-2],Entities[EntLen-3],Entities[EntLen-4],Entities[EntLen-5]]
-
-	print()
-	print(EntitiesTop5)
-	writer.writerow(EntitiesOnly)
+	#EntLen = len(Entities)
+	#EntitiesTop5 = [Entities[EntLen-1],Entities[EntLen-2],Entities[EntLen-3],Entities[EntLen-4],Entities[EntLen-5]]
+	print("+++++++")
+	print(EntitiesOnly)
+	#print(EntitiesTop5)
+	writer.write(EntitiesOnly+'\n')
